59f9d191fb8237e9dbc13b52935f03f3a4f50698
vemseb45 <olimacgamer1236@gmail.com>
2026-03-31 11:52:08 -0500

    get usuarios sin password

--- src/modules/users/user.model.js
+++ src/modules/users/user.model.js
@@ -1,7 +1,7 @@
 const db = require("../../config/db");
 
 const getAllUsers = async () => {
-    const result = await db.query("SELECT * FROM users");
+    const result = await db.query("SELECT id,name,email, role FROM users;");
     return result.rows;
 };
 
