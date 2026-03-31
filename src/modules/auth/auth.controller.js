2a1b424ae0b8cffc1258412f1b4a79db4a09077e
vemseb45 <olimacgamer1236@gmail.com>
2026-03-31 15:33:07 -0500

    nuevas dependencias

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,6 +1,8 @@
 const authService = require("./auth.service");
 const asyncHandler = require("../../middlewares/asyncHandler");
 
+//NO COLOCAR ROLE YA QUE SE PUEDE SOBRE ESCRBIR Y UN CLIENTE SE PUEDE
+//CONVERTIR EN ADMIN
 exports.register = asyncHandler(async (req, res) => {
 
     const { name, email, password } = req.body;
